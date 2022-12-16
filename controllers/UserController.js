2b9fbec608d53d138227113643cb3cac2d724e92
mohamedsek <mohamed.sekkoume@gmail.com>
2022-12-16 10:45:55 +0100

    create User Controller

---
+++ controllers/UserController.js
@@ -0,0 +1,30 @@
+const UserModel = require('../models/UserModel')
+
+exports.getuser = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
+exports.adduser = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
+exports.updateuser = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
+exports.deleteuser = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
