f67067673e1565e7174196c99598eb8c5ed4cbfb
mohamedsek <mohamed.sekkoume@gmail.com>
2023-01-11 15:31:56 +0100

    modify api route

--- routes/MoviesRouter.js
+++ routes/MoviesRouter.js
@@ -1,9 +1,9 @@
 const MoviesRouter = require('express').Router();
 const MoviesController = require('../controllers/MoviesController')
 
-MoviesRouter.get('/all',MoviesController.getAllMovies)
+MoviesRouter.get('/',MoviesController.getAllMovies)
 MoviesRouter.get('/:id',MoviesController.getMovie)
-MoviesRouter.post('/addMovie',MoviesController.addMovie)
+MoviesRouter.post('/',MoviesController.addMovie)
 MoviesRouter.put('/:id',MoviesController.updateMovie)
 MoviesRouter.delete('/:id',MoviesController.deleteMovie)
 
